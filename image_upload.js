283f69db8636a4e478917d8e36f19a62275bc8db
Ronald Williams <ronaldnwilliams@gmail.com>
2018-08-06 12:12:03 -0500

    :construction: Adding state and making preview button only appear if image selected.

--- image_upload.js
+++ image_upload.js
@@ -5,12 +5,19 @@ const e = React.createElement;
 class ImageUpload extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { 
+      fileSelected: false,
+      file: ''
+    }
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(e) {
-    console.log(e.target.files[0]);
+    this.setState({
+      fileSelected: true,
+      file: e.target.files[0]
+    })
   }
 
   handleSubmit(e) {
@@ -19,16 +26,19 @@ class ImageUpload extends React.Component {
   }
 
   render() {
+    let previewButton = this.state.fileSelected ? 
+      React.createElement('button',
+      { type: 'submit', onClick: this.handleSubmit },
+      'Print Preview') : 
+      React.createElement('p', {}, 'Select an image to uplaod');
+
     return React.createElement(
       'form',
       { onSubmit: this.handleSubmit },
       React.createElement(
         'input',
         {type: 'file', accept: 'image/*', onChange: this.handleChange }),
-      React.createElement(
-        'button',
-        { type: 'submit', onClick: this.handleSubmit },
-        'Print Preview'),
+      previewButton,
     );
   }
 }
